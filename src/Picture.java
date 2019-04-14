8e8b5e288d8c5e4739a8b50df68d1737a4a43b5d
jasmine <yuejian@seas.upenn.edu>
2019-04-14 16:59:50 -0700

    Made a small change to picture java

--- src/Picture.java
+++ src/Picture.java
@@ -12,13 +12,13 @@ public class Picture {
     private String c5;
     private String pictureFileName;
     
-    public Picture(String cOne, String cTwo, String cThree, String cFour, String cFive, int picRef) {
-	c1 = cOne;
-	c2 = cTwo;
-	c3 = cThree;
-	c4 = cFour;
-	c5 = cFive;
+    
+    
+    public Picture(int picRef) {
 	pictureFileName = "p"+picRef+".jpg";
+	//TODO: call the file reader from Zeke1830; 
+	//TODO: call the TopN color analzer from yuhong then assign the returned value to c1 to c5;
     }
+    
     
 }
