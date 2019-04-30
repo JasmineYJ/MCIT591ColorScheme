da0eadacb35e95ada39da1204beeab385ff142c5
yuhong <yuhongdu@seas.upenn.edu>
2019-04-29 21:02:56 -0400

    Update

--- src/ColorPictureLibrary.java
+++ src/ColorPictureLibrary.java
@@ -4,89 +4,94 @@ import java.io.*;
 
 /**
  * Recommend similar picture by comparing the color names
+ * 
  * @author jasminejian
  *
  */
 
 public class ColorPictureLibrary {
-    public HashMap<Integer, Picture> colorToPicture;
-    public String libraryColorResult = "pictureLibraryResult.csv";
-    private int numPictures = 100; // TODO: remember to update this number when we expand the library;
+	public HashMap<Integer, Picture> colorToPicture;
+	public String libraryColorResult = "pictureLibraryResult.csv";
+	private int numPictures = 100; // TODO: remember to update this number when we expand the library;
 
-    // TODO: assign a threshold that we can adjust as long as the variances is less
-    // than that, we can assign the color name, see if any exception;
+	// TODO: assign a threshold that we can adjust as long as the variances is less
+	// than that, we can assign the color name, see if any exception;
 
-    // Constructor which loop through all pictures and write to CSV. This line will retire once we have everything running.
-    public ColorPictureLibrary() {
-	ColorNameLibrary cNames = new ColorNameLibrary();
-	colorToPicture = new HashMap<Integer, Picture>();
+	// Constructor which loop through all pictures and write to CSV. This line will
+	// retire once we have everything running.
+	public ColorPictureLibrary() {
+		ColorNameLibrary cNames = new ColorNameLibrary();
+		colorToPicture = new HashMap<Integer, Picture>();
 
-	// read through the picture library and store the result to the CSV file.
-	for (Integer i = 1; i <= numPictures; i++) {
-	    Picture pi = new Picture(i, cNames);
-	    colorToPicture.put(i, pi);
-	}
-	writeColorToCSV(colorToPicture);
-    }
-
-    // Method that returns a similar picture as user input. Should be called by system output. 
-    public String similarPic(int[][] userPicture) {
-	Integer ref = 0;
-	int benchmark = 0;
-	Picture userP = new Picture(userPicture);
-
-	for (Integer i : colorToPicture.keySet()) {
-	    Integer count = 0;
-	    Picture thisP = colorToPicture.get(i);
-
-	    if (thisP.getC1() == userP.getC1() | thisP.getC1() == userP.getC2() ) {
-		count+=100;
-	    }
-	    
-	    if (thisP.getC2() == userP.getC2() | thisP.getC2() == userP.getC3()) {
-		count+=80;
-	    }
-	    if (thisP.getC3() == userP.getC3() | thisP.getC3() == userP.getC4()) { //TODO: Add try-catch block for single color;
-		count+=60;
-	    }
-	    if (thisP.getC4() == userP.getC4() | thisP.getC4() == userP.getC5()) { //TODO: Add try-catch block for single color;
-		count+=40;
-	    }
-	    if (thisP.getC5() == userP.getC5()) { //TODO: Add try-catch block for single color;
-		count+=20;
-	    }
-  
-	    if (count > benchmark) {
-		ref = i;
-		benchmark = count;
-	    }
+		// read through the picture library and store the result to the CSV file.
+		for (Integer i = 1; i <= numPictures; i++) {
+			Picture pi = new Picture(i, cNames);
+			colorToPicture.put(i, pi);
+		}
+		writeColorToCSV(colorToPicture);
 	}
 
-	Picture similarPic = colorToPicture.get(ref);
-	return similarPic.getPictureFileName();
-    }
+	// Method that returns a similar picture as user input. Should be called by
+	// system output.
+	public String similarPic(int[][] userPicture) {
+		Integer ref = 0;
+		int benchmark = 0;
+		Picture userP = new Picture(userPicture);
 
-    // Method that write the resulted hash map to the CSV file.
-    public void writeColorToCSV(HashMap<Integer, Picture> pictureLib) {
-	File out = new File(libraryColorResult);
+		for (Integer i : colorToPicture.keySet()) {
+			Integer count = 0;
+			Picture thisP = colorToPicture.get(i);
 
-	try (PrintWriter pw = new PrintWriter(out)) {
+			if (thisP.getC1() == userP.getC1() | thisP.getC1() == userP.getC2()) {
+				count += 100;
+			}
 
-	    // Prints each line in answers
-	    for (Integer i : pictureLib.keySet()) {
-		pw.print(i + ",");
-		Picture thisP = pictureLib.get(i);
-		pw.print(thisP.getC1() + ",");
-		pw.print(thisP.getC2() + ",");
-		pw.print(thisP.getC3() + ",");
-		pw.print(thisP.getC4() + ",");
-		pw.print(thisP.getC5() + ",");
-	    }
+			if (thisP.getC2() == userP.getC2() | thisP.getC2() == userP.getC3()) {
+				count += 80;
+			}
+			if (thisP.getC3() == userP.getC3() | thisP.getC3() == userP.getC4()) { // TODO: Add try-catch block for
+																					// single color;
+				count += 60;
+			}
+			if (thisP.getC4() == userP.getC4() | thisP.getC4() == userP.getC5()) { // TODO: Add try-catch block for
+																					// single color;
+				count += 40;
+			}
+			if (thisP.getC5() == userP.getC5()) { // TODO: Add try-catch block for single color;
+				count += 20;
+			}
 
-	} catch (IOException e) {
-	    e.printStackTrace();
-	    System.out.println("Could not write the File out.  Check permissions, or contact course staff for help");
+			if (count > benchmark) {
+				ref = i;
+				benchmark = count;
+			}
+		}
+
+		Picture similarPic = colorToPicture.get(ref);
+		return similarPic.getPictureFileName();
 	}
-    }
+
+	// Method that write the resulted hash map to the CSV file.
+	public void writeColorToCSV(HashMap<Integer, Picture> pictureLib) {
+		File out = new File(libraryColorResult);
+
+		try (PrintWriter pw = new PrintWriter(out)) {
+
+			// Prints each line in answers
+			for (Integer i : pictureLib.keySet()) {
+				pw.print(i + ",");
+				Picture thisP = pictureLib.get(i);
+				pw.print(thisP.getC1() + ",");
+				pw.print(thisP.getC2() + ",");
+				pw.print(thisP.getC3() + ",");
+				pw.print(thisP.getC4() + ",");
+				pw.print(thisP.getC5() + ",");
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Could not write the File out.  Check permissions, or contact course staff for help");
+		}
+	}
 
 }
--- src/Picture.java
+++ src/Picture.java
@@ -7,108 +7,108 @@
  */
 
 public class Picture {
-    private String c1;
-    private String c2;
-    private String c3;
-    private String c4;
-    private String c5;
-    private String pictureFileName;
-    private ColorNameLibrary CML;
-    private int numOfColor = 5; // TODO: remember to update this based on UI read;
-    private int roundOfIteration = 30; // TODO: change this number based on runtime and accuracy;
+	private String c1;
+	private String c2;
+	private String c3;
+	private String c4;
+	private String c5;
+	private String pictureFileName;
+	private ColorNameLibrary CML;
+	private int numOfColor = 5; // TODO: remember to update this based on UI read;
+	private int roundOfIteration = 30; // TODO: change this number based on runtime and accuracy;
 
-    public Picture(int picRef, ColorNameLibrary CML) {
-	pictureFileName = "p" + picRef + ".jpg";
+	public Picture(int picRef, ColorNameLibrary CML) {
+		pictureFileName = "p" + picRef + ".jpg";
 
-	ImageReading reader = new ImageReading(pictureFileName);
-	int[][] colorResult = reader.getImageRGB();
-	int pixelNum = reader.getPixelNum();
-	KmeansCalculator kC = new KmeansCalculator(pixelNum, numOfColor, colorResult);
+		ImageReading reader = new ImageReading(pictureFileName);
+		int[][] colorResult = reader.getImageRGB();
+		int pixelNum = reader.getPixelNum();
+		KmeansCalculator kC = new KmeansCalculator(pixelNum, numOfColor, colorResult);
 
-	int[][] firstCenter = kC.firstPathCenter();
-	int[] label = kC.lablePixels(firstCenter);
+		int[][] firstCenter = kC.firstPathCenter();
+		int[] label = kC.lablePixels(firstCenter);
 
-	// int[][] is the result containing the top N colors. Each row stores a color
-	int[][] result = new int[numOfColor][3];
+		// int[][] is the result containing the top N colors. Each row stores a color
+		int[][] result = new int[numOfColor][3];
 
-	int num = 0;
-	while (num < roundOfIteration) {
-	    result = kC.calculateCenter(label);
-	    label = kC.lablePixels(result);
-	    num++;
+		int num = 0;
+		while (num < roundOfIteration) {
+			result = kC.calculateCenter(label);
+			label = kC.lablePixels(result);
+			num++;
+		}
+
+		for (int i1 = 0; i1 < numOfColor; i1++) {
+			int rValue = result[i1][0];
+			int gValue = result[i1][1];
+			int bValue = result[i1][2];
+			String colorName = CML.getColorName(rValue, gValue, bValue);
+			if (i1 == 1) {
+				c1 = colorName;
+			}
+			if (i1 == 2) {
+				c2 = colorName;
+			}
+			if (i1 == 3) {
+				c3 = colorName;
+			}
+			if (i1 == 4) {
+				c4 = colorName;
+			} else {
+				c5 = colorName;
+			}
+		}
 	}
 
-	for (int i1 = 0; i1 < numOfColor; i1++) {
-	    int rValue = result[i1][0];
-	    int gValue = result[i1][1];
-	    int bValue = result[i1][2];
-	    String colorName = CML.getColorName(rValue, gValue, bValue);
-	    if (i1 == 1) {
-		c1 = colorName;
-	    }
-	    if (i1 == 2) {
-		c2 = colorName;
-	    }
-	    if (i1 == 3) {
-		c3 = colorName;
-	    }
-	    if (i1 == 4) {
-		c4 = colorName;
-	    } else {
-		c5 = colorName;
-	    }
+	// a different constructor which takes the result from Main result of the user
+	// input
+	public Picture(int[][] userR) {
+		int num = 0;
+
+		for (int i1 = 0; i1 < numOfColor; i1++) {
+			int rValue = userR[i1][0];
+			int gValue = userR[i1][1];
+			int bValue = userR[i1][2];
+			String colorName = CML.getColorName(rValue, gValue, bValue);
+			if (i1 == 1) {
+				c1 = colorName;
+			}
+			if (i1 == 2) {
+				c2 = colorName;
+			}
+			if (i1 == 3) {
+				c3 = colorName;
+			}
+			if (i1 == 4) {
+				c4 = colorName;
+			} else {
+				c5 = colorName;
+			}
+		}
 	}
-    }
 
-    // a different constructor which takes the result from Main result of the user
-    // input
-    public Picture(int[][] userR) {
-	int num = 0;
-
-	for (int i1 = 0; i1 < numOfColor; i1++) {
-	    int rValue = userR[i1][0];
-	    int gValue = userR[i1][1];
-	    int bValue = userR[i1][2];
-	    String colorName = CML.getColorName(rValue, gValue, bValue);
-	    if (i1 == 1) {
-		c1 = colorName;
-	    }
-	    if (i1 == 2) {
-		c2 = colorName;
-	    }
-	    if (i1 == 3) {
-		c3 = colorName;
-	    }
-	    if (i1 == 4) {
-		c4 = colorName;
-	    } else {
-		c5 = colorName;
-	    }
+	public String getC1() {
+		return c1;
 	}
-    }
 
-    public String getC1() {
-	return c1;
-    }
+	public String getC2() {
+		return c2;
+	}
 
-    public String getC2() {
-	return c2;
-    }
+	public String getC3() {
+		return c3;
+	}
 
-    public String getC3() {
-	return c3;
-    }
+	public String getC4() {
+		return c4;
+	}
 
-    public String getC4() {
-	return c4;
-    }
+	public String getC5() {
+		return c5;
+	}
 
-    public String getC5() {
-	return c5;
-    }
-
-    public String getPictureFileName() {
-	return pictureFileName;
-    }
+	public String getPictureFileName() {
+		return pictureFileName;
+	}
 
 }
