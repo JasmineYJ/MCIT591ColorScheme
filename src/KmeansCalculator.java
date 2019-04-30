c56c447907d048e5773f223a07ad6c1573593c6c
yuhong <yuhongdu@seas.upenn.edu>
2019-04-29 21:14:18 -0400

    Update to calculator

--- src/KmeansCalculator.java
+++ src/KmeansCalculator.java
@@ -1,5 +1,6 @@
 
 import java.awt.Color;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -27,6 +28,7 @@ public class KmeansCalculator {
 		this.numOfCenters = numOfCenters;
 		imageRGBs = inputRGBs;
 	}
+	
 
 	/**
 	 * This is the method to generate the initial center point for each cluster,
@@ -91,11 +93,38 @@ public class KmeansCalculator {
 		}
 		return firstCenters;
 	}
+	
 
 	/**
+	 * This is the method to generate the RGB value pattern. Label the max value
+	 * from RGB as 3, the min value as 1, and the medium value as 2.
+	 */
+	public int[] getRGBPattern(int[] rgb) {
+		int[] RGBPattern = new int[3];
+		int[] rgbCopy = new int[3];
+		for (int i = 0; i < 3; i++) {
+			rgbCopy[i] = rgb[i];
+		}
+		Arrays.sort(rgb);
+		for (int i = 0; i < 3; i++) {
+			int temp = rgbCopy[i];
+			for (int j = 0; j < 3; j++) {
+				if (temp == rgb[j]) {
+					RGBPattern[i] = j + 1;
+				}
+			}
+		}
+		return RGBPattern;
+	}
+
+	
+	/**
 	 * This Method label each pixel based on their "color Euclidean distance" from
-	 * the cluster centers, so each pixel will be associated with a color it's
-	 * closest to
+	 * the cluster centers, as well as their "pattern distance" from the center
+	 * color. The "pattern distance" is utilized to get the distinct color from an
+	 * image, where this distinct color has a small portion; also used to minimize
+	 * the chance of getting a grey color. So each pixel will be associated with a
+	 * color it's closest to
 	 */
 	public int[] lablePixels(int[][] centers) {
 		int[] label = new int[totalPixels];
@@ -107,18 +136,21 @@ public class KmeansCalculator {
 			target[0] = imageRGBs[i][0];
 			target[1] = imageRGBs[i][1];
 			target[2] = imageRGBs[i][2];
+			int[] targetPattern = getRGBPattern(target);
 			for (int j = 0; j < numOfCenters; j++) {
 				int[] centerColor = new int[3];
 				centerColor[0] = centers[j][0];
 				centerColor[1] = centers[j][1];
 				centerColor[2] = centers[j][2];
+				int[] centerPattern = getRGBPattern(centerColor);
 				/**
 				 * get the distance between this two points
 				 */
 				int temp = 0;
 				int distance = 0;
 				for (int x = 0; x < 3; x++) {
-					temp = (int) Math.pow((target[x] - centerColor[x]), 2);
+					temp = (int) Math.pow((target[x] - centerColor[x]), 2)
+							+ (int) Math.pow((targetPattern[x] - centerPattern[x]), 2) * 25000;
 					distance = distance + temp;
 				}
 				/**
@@ -133,6 +165,7 @@ public class KmeansCalculator {
 		}
 		return label;
 	}
+	
 
 	/**
 	 * This method is to re-calculate(update) the center of each cluster, after each
@@ -165,37 +198,38 @@ public class KmeansCalculator {
 		}
 		return newCenter;
 	}
-	
+
 	
 	/**
-	 * This method put every method needed for calculation together; 
+	 * This method put every method needed for calculation together;
 	 */
-	public int[][] calculateColor(){
+	public int[][] calculateColor() {
 		int[][] firstCenter = firstPathCenter();
 		int[] label = lablePixels(firstCenter);
 		int i = 0;
 		int[][] result = new int[numOfCenters][3];
-		
+
 		/**
-		 * int iterationRounds controls how many rounds of iteration in the calculation process;
-		 * The bigger int iterationRounds is, the more accurate the result would be, but at the same time,
-		 * the longer the run time would be. The int iteratoinRounds here is dependent on the size of the
-		 * image which is to be calculated;
+		 * int iterationRounds controls how many rounds of iteration in the calculation
+		 * process; The bigger int iterationRounds is, the more accurate the result
+		 * would be, but at the same time, the longer the run time would be. The int
+		 * iteratoinRounds here is dependent on the size of the image which is to be
+		 * calculated;
 		 */
 		int iterationRounds = 0;
 		if (totalPixels < 1000 * 1000) {
-			iterationRounds = 25;
-		}else if (totalPixels >= 1000 * 1000 && totalPixels <= 3000 * 3000){
 			iterationRounds = 15;
-		}else {
-			iterationRounds = 5;
+		} else if (totalPixels >= 1000 * 1000 && totalPixels <= 3000 * 3000) {
+			iterationRounds = 8;
+		} else {
+			iterationRounds = 3;
 		}
+
 		while (i < iterationRounds) {
 			result = calculateCenter(label);
 			label = lablePixels(result);
 			i++;
 		}
-		
 		return result;
 	}
 
@@ -213,15 +247,15 @@ public class KmeansCalculator {
 		int pixelNum = image.getPixelNum();
 
 		KmeansCalculator k = new KmeansCalculator(pixelNum, topN, testI);
-		
+
 		int[][] result = k.calculateColor();
 
-		// here is to print the result color
-//		for (int i = 0; i < topN; i++) {
-//			System.out.print(result[i][0] + ",");
-//			System.out.print(result[i][1] + ",");
-//			System.out.print(result[i][2]);
-//			System.out.println();
-//		}
+		// here is to print the result color, for test only
+		for (int i = 0; i < topN; i++) {
+			System.out.print(result[i][0] + ",");
+			System.out.print(result[i][1] + ",");
+			System.out.print(result[i][2]);
+			System.out.println();
+		}
 	}
 }
