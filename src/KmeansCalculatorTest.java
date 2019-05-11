5e8073f1201fcf677d36fcce76c5691245097ffd
yuhongdu <49623723+yuhongdu@users.noreply.github.com>
2019-05-11 19:11:25 -0400

    Add Unit Test

--- src/KmeansCalculatorTest.java
+++ src/KmeansCalculatorTest.java
@@ -63,4 +63,39 @@ class KmeansCalculatorTest {
 			}
 		}
 	}
+	
+	@Test
+	void testGetRGBPattern() {
+		/**
+		 * "test_image.jpg" is the name of the image used for testing
+		 */
+		ImageReading i = new ImageReading("test_image.jpg");
+		int[][] testI = i.getImageRGB();
+		int pixelNum = i.getPixelNum();
+		KmeansCalculator k = new KmeansCalculator(pixelNum, 5, testI);
+		int[] testColor = {202,135,200};
+		int[] label = k.getRGBPattern(testColor);
+		assertEquals(label[0],3);
+		assertEquals(label[1],1);
+		assertEquals(label[2],2);
+	}
+	
+	@Test
+	void testCalculateColor() {
+		/**
+		 * "test_image.jpg" is the name of the image used for testing
+		 */
+		ImageReading i = new ImageReading("test_image.jpg");
+		int[][] testI = i.getImageRGB();
+		int pixelNum = i.getPixelNum();
+		KmeansCalculator k = new KmeansCalculator(pixelNum, 5, testI);
+		int[][] result = k.calculateColor();
+		for (int x = 0; x < 5; x++) {
+			for (int y = 0; y < 3; y++) {
+				int value = result[x][y];
+				assertTrue("Out of color max value", 256 >= value);
+				assertTrue("Out of color min value", 0 <= value);
+			}
+		}
+	}
 }
